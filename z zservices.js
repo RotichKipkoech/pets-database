c13b189fa48be4df6bd1b6cc05a8430ad63eae9c
Alex-gikungu <alexigikugnu.012@gmail.com>
2023-10-16 15:09:32 +0300

    initiall commit

---
+++ z zservices.js
@@ -0,0 +1,165 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+const ServicesSection = () => {
+  return (
+    <>
+    
+    <Header/>
+    
+    <div className="main">
+      <div className="image">
+        <img src="./basin1.jpeg" alt="image" />
+        <h2>Washroom Toilet & Sink Installation</h2>
+        <p>we offer sanitary appliances  repairs and installations. We also install and repair kitchen sinks and washhand basins . We make sure that all our work is done to the highest standards of safety and efficiency.</p>
+      </div>
+
+      {/* Repeat the following block for each service */}
+      <div className="image">
+        <img src="/toilet.jpeg" alt="image" />
+        <h2>Washroom Toilet Repair & Installation</h2>
+        <p>we offer water closset repairs and installations. We make sure that all our work is done to the highest standards of safety and efficiency.</p>
+      </div>
+
+      
+      <div className="image">
+        <img src="./toile2.jpeg" alt="image"/>
+        <h2>Washroom  Basin Repair & Installation</h2>
+        <p> we offer Grab rail for the elderly and disabbled at an affordable cost.We also install arabic showers and telephonic showers . 
+             We make sure that all our work is done to the highest standards of safety and efficiency.</p>
+      </div>
+
+
+
+
+
+
+
+
+      <div className="image">
+        <img src="./toile3.jpeg" alt="image"/>
+        <h2> Table Top Wash Hand Basins Installation</h2>
+        <p> we offer sinks Installation. We can repair any existing  or broken sinks 
+            install new ones either in large or small scale apartments.
+             We make sure that all our work is done to the highest standards of safety and efficiency.</p>
+      </div>
+    
+      <div className="image">
+        <img src="./toile4.jpeg" alt="image"/>
+        <h2> Table Top Wash Hand Basins Installation</h2>
+        <p> we offer sinks Installation. We can repair any existing  or broken sinks 
+            install new ones either in large or small scale apartments.
+             We make sure that all our work is done to the highest standards of safety and efficiency.</p>
+      </div>
+      
+      <div className="image">
+        <img src="./plumb4.jpeg" alt="image"/>
+        <h2> Table Top Wash Hand Basins Installation</h2>
+        <p> we offer sinks Installation. We can repair any existing  or broken sinks 
+            install new ones  in large  apartments and institutions .
+             We make sure that all our work is done to the highest standards of safety and efficiency.</p>
+      </div>
+
+      <div className="image">
+        <img src="./plumb1.jpeg" alt="image"/>
+        <h2>Solar Water Heating System Installation</h2>
+        <p> we offer solar water heating system installation installations. We can repair any existing  or 
+            install new ones to update your bathroom.
+             We make sure that all our work is done to the highest standards of safety and efficiency.</p>
+      </div>
+
+      <div className="image">
+        <img src="./water2.jpeg" alt="image"/>
+        <h2>Irrigation Piping & Installation </h2>
+        <p> we offer irrigation piping and also maintenance of the irrigation piping 
+            in large scale farming .
+             We make sure that all our work is done to the highest standards of safety and efficiency.</p>
+      </div>
+
+      <div className="image">
+        <img src="./pointing.jpeg" alt="image"/>
+        <h2>Pointing Installation</h2>
+        <p> we offer pointing  installations. To under construction sites whether large or small scale and perfect more than expected.
+             We make sure that all our work is done to the highest standards of safety and efficiency.</p>
+      </div>
+
+      <div className="image">
+        <img src="./pro4.jpeg" alt="image"/>
+        <h2>Meter Installation & Repair</h2>
+        <p> we offer meter box  repairs and installations. We offer installation of the meter box to apartments under constructions 
+            and also leaking meter box.
+             We make sure that all our work is done to the highest standards of safety and efficiency.</p>
+      </div>
+
+      <div className="image">
+        <img src="./pro5.jpeg" alt="image"/>
+        <h2>Pressure Piping Installation & Repair </h2>
+        <p> we offer pressure piping repairs and installations. We install pressure piping on the roof top to increase 
+            the supply of water in your appartment and also forter storage .
+             We make sure that all our work is done to the highest standards of safety and efficiency.</p>
+      </div>
+
+      <div className="image">
+        <img src="./pro6.jpeg" alt="image"/>
+        <h2>Sewage Pipeline Repair & Installation</h2>
+        <p> we offer sewage pipelining repairs and installations. We can repair any existing liking  or 
+            install new ones to make sure proper drainage in your appartment .
+             We make sure that all our work is done to the highest standards of safety and efficiency.</p>
+      </div>
+      <div className="image">
+        <img src="./pro1.jpeg" alt="image"/>
+        <h2>Pointing Installation</h2>
+        <p> we offer pointing  installations. To under construction sites whether large or small scale and perfect more than expected.
+             We make sure that all our work is done to the highest standards of safety and efficiency.</p>
+      </div>
+    
+  <div className="image">
+    <img src="./plumb2.jpeg" alt="image"/>
+    <h2>Dental Machine Installation</h2>
+    <p> we offer Dental Machine Installation. We can repair any existing faucets or 
+        install new ones either in large or small scale hospitals.
+         We make sure that all our work is done to the highest standards of safety and efficiency.</p>
+  </div>
+      <div className="image">
+        <img src="./plumb3.jpeg" alt="image"/>
+        <h2>Pressure Piping Installation & Repair </h2>
+        <p> we offer pressure piping repairs and installations. We install pressure piping on the roof top to increase 
+            the supply of water in your appartment and also forter storage .
+             We make sure that all our work is done to the highest standards of safety and efficiency.</p>
+      </div>
+      
+
+      <div className="image">
+        <img src="./wash basin.jpeg" alt="image"/>
+        <h2> Table Top Wash Hand Basins Installation</h2>
+        <p> we offer table wash hand basin repairs and installations. We can repair any leaking basin or 
+            install new ones to update your kitchen or bathroom.
+             We make sure that all our work is done to the highest standards of safety and efficiency.</p>
+      </div>
+
+
+      <div className="image">
+        <img src="./biogas.jpeg" alt="image"/>
+        <h2>Bio Digestor Construction & Repair</h2>
+        <p> we offer bio digestor construction  and repairs. We can construct a new bio digestor and even 
+         repair an existing to update your biogas system .
+             We make sure that all our work is done to the highest standards of safety and efficiency.</p>
+      </div>
+
+
+      <div className="image">
+        <img src="./bio2 gas.jpeg" alt="image"/>
+        <h2>Bio Digestor Construction & Repair</h2>
+        <p> we offer bio digestor construction  and repairs. We can construct a new bio digestor and even 
+         repair an existing to update your biogas system .
+             We make sure that all our work is done to the highest standards of safety and efficiency.</p>
+      </div>
+      {/* Add more service sections as needed */}
+    </div>
+    <Footer/>
+    </>
+  );
+};
+
+export default ServicesSection;
